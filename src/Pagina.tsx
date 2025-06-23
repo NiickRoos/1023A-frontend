a5ee84bddd35580c2751ec9cf9f3fe1fb4331b51
niickroos <nicole.roos@estudante.ifms.edu.br>
2025-06-23 11:12:49 -0300

    aula 2306

--- src/Pagina.tsx
+++ src/Pagina.tsx
@@ -1,4 +1,5 @@
 import { useState  } from "react"
+import './Pagina.css'
 interface ProdutosState{
     id:number,
     nome:string,
@@ -24,7 +25,7 @@ function Pagina(){
     function TrataCadastro(event: React.FormEvent<HTMLFormElement> ){
        event.preventDefault();
        //cria um novo produto
-       const novoProduto:ProdutosState =  {
+       const novoProdutos:ProdutosState =  {
           id:parseInt(id),
           nome:nome,
           preco:parseFloat(preco),
@@ -33,6 +34,7 @@ function Pagina(){
        
 
        //Adicionar esse novo produto no vetor/Array de produtos 
+       setProdutos([...produtos,novoProdutos])
     }
     function trataid(event:React.ChangeEvent<HTMLInputElement>){
         setId(event.target.value)
@@ -71,12 +73,12 @@ function Pagina(){
         </header>
 
         <main>
-            <h1> Bem vindo(a) ao meu site de cadrastro e listagem</h1>
+            
 
             <div className="container-listagem"> 
                 
                     {/*colocar chaves para virar javascript*/}
-                    <h2>Lista de componentes cadastrados</h2>
+                    
                 {produtos.map(produto=>{
                     return(
                         <div className="produto-container">
@@ -101,12 +103,12 @@ function Pagina(){
             <div className="container-cadastro">
                 <h2>Cadastre suas informações</h2>
                 <form onSubmit={TrataCadastro}>
-                <label>id</label>
-                <input type="text" name="id" id="id" onChange={trataid}/>
+               
+                <input type="text" name="id" id="id" onChange={trataid}placeholder="id"/>
               
-                <input type="text" name="nome" id="nome" onChange={tratanome}/>
-                <input type="text" name="preco" id="preco" onChange={tratapreco}/>
-                <input type="text" name="categoria" id="categoria" onChange={tratacategoria}/>
+                <input type="text" name="nome" id="nome" onChange={tratanome}placeholder="nome"/>
+                <input type="text" name="preco" id="preco" onChange={tratapreco}placeholder="preço"/>
+                <input type="text" name="categoria" id="categoria" onChange={tratacategoria}placeholder="categoria"/>
                 <input type="submit" value="cadastrar" />
                  
                 </form>
@@ -115,14 +117,7 @@ function Pagina(){
 
             </div>
         </main>
-        <footer>
-
-            Para mais informações entre em contato com a gente.
-            <li>90028922</li>
-            <li>facebook</li>
-            <li>Instagram</li>
-
-        </footer>
+       
         </>
      )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 // import Container from './Container.tsx'
- import Pagina  from './pagina'
+ import Pagina  from './Pagina'
 
 
 
